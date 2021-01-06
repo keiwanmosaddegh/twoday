f498e0c47241da982119859e96f80c3c04173736
Keiwan Mosaddegh <keiwan.mosaddegh@gmail.com>
2021-01-06 23:00:04 +0100

    Added placeholder for quarterly stats in details page

--- lib/screens/details_screen/detailsScreen.dart
+++ lib/screens/details_screen/detailsScreen.dart
@@ -10,7 +10,12 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return BlocBuilder<HabitBloc, HabitState>(
+        builder: (BuildContext context, HabitState state) {
+      final habit = (state as HabitsLoadSuccess)
+          .habits
+          .firstWhere((habit) => habit.id == id, orElse: () => null);
+      return Scaffold(
         appBar: PreferredSize(
           preferredSize: Size.fromHeight(80),
           child: AppBar(
@@ -19,7 +24,7 @@ class DetailsScreen extends StatelessWidget {
             flexibleSpace: Padding(
                 padding: EdgeInsets.only(top: 60, left: 20),
                 child: Text(
-                  "Hello",
+                  habit.task,
                   style: TextStyle(
                       fontSize: 30,
                       fontWeight: FontWeight.w600,
@@ -27,12 +32,67 @@ class DetailsScreen extends StatelessWidget {
                 )),
           ),
         ),
-        body: BlocBuilder<HabitBloc, HabitState>(
-            builder: (BuildContext context, HabitState state) {
-          final habit = (state as HabitsLoadSuccess)
-              .habits
-              .firstWhere((habit) => habit.id == id, orElse: () => null);
-          return Text("habit: ${habit.id}");
-        }));
+        body: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          margin: EdgeInsets.only(left: 18, right: 18, bottom: 20, top: 20),
+          child: Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+              Text(
+              "Overview",
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.grey[850]),
+            ),
+                SizedBox(height: 10,),
+                Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Q1",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.grey[850]),
+                        ),
+                        Text(
+                          "Q2",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.grey[850]),
+                        ),
+                        Text(
+                          "Q3",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.grey[850]),
+                        ),
+                        Text(
+                          "Q4",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.grey[850]),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
