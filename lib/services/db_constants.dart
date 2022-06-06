a3f8e38c125c12c354e255e81c312251ff491d02
Keiwan Mosaddegh <keiwan.mosaddegh@gmail.com>
2022-06-06 23:02:25 +0200

    Extract database constants into separate, global file

---
+++ lib/services/db_constants.dart
@@ -0,0 +1,12 @@
+const String TABLE_HABIT = "habit";
+const String HABIT_COLUMN_ID = "id";
+const String HABIT_COLUMN_TASK = "task";
+const String HABIT_COLUMN_DONE = "done";
+const String HABIT_COLUMN_CURRENT_STREAK = "current_streak";
+const String HABIT_COLUMN_LONGEST_STREAK = "longest_streak";
+const String HABIT_COLUMN_DAYS_SINCE_LAST_DONE = "days_since_last_done";
+
+const String TABLE_HABITRECORDS = "habit_records";
+const String HABITRECORDS_COLUMN_ID = "id";
+const String HABITRECORDS_COLUMN_HABIT_ID = "habit_id";
+const String HABITRECORDS_COLUMN_DATE = "date";
